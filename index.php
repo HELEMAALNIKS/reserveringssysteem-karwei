faa07f3affdbc31e94aea35a05f1cf790c5d94ea
HELEMAAL_NIKS <jaron@hmcnetwork.nl>
2018-12-12 13:41:04 +0100

    12 December

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jaron
+ * Date: 10/12/2018
+ * Time: 13:47
+ */
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Karwei Krimpen</title>
+</head>
+<body>
+    <header>
+        <h1>Karwei</h1>
+    </header>
+    <footer>
+
+    </footer>
+</body>
+</html>
